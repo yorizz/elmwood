8373a14a8fea2d84d9a04b7ef800fd8bd11ac4e7
yorizz <yorizz@gmail.com>
2024-10-25 15:32:28 +0100

    fixed typo

--- src/utils/dbconfig.js
+++ src/utils/dbconfig.js
@@ -5,5 +5,5 @@ module.exports = {
 	database: process.env.DB_NAME,
 	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
-	post: process.env.DB_PORT,
+	port: process.env.DB_PORT,
 };
